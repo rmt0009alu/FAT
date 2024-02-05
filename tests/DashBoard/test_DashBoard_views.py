931635c7286c853bd19709cd2c159cbc2c83878c
rmt0009alu <rodmertov@gmail.com>
2024-02-05 19:02:52 +0100

    TDD. Tests DashBoard views

--- tests/DashBoard/test_DashBoard_views.py
+++ tests/DashBoard/test_DashBoard_views.py
@@ -1,16 +1,6 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
-# Lo importo como 'tz' para no confundir con timezone de datetime
-from django.utils import timezone as tz
-from datetime import timedelta
-from DashBoard.models import StockComprado, StockSeguimiento
+from django.contrib.auth.models import User# Lo importo como 'tz' para no confundir con timezone de datetime
 from log.logger.logger import get_logger_dashboard
-# Refactoring:
-# ------------
-# No puedo usar ValueError ni ValidationError, el error
-# que se devuelve con la fecha y bd no válidas es IntegrityError
-# from django.core.exceptions import ValidationError
-from django.db.utils import IntegrityError
 
 
 # Idea original de NuclearPeon:
@@ -39,54 +29,40 @@ class TestDashBoardViews(TestCase):
     # NOTA: Posibles códigos HTML para algunos de los tests de esta clase.
     # 200 OK, 201 Created, 204 No Content, 302 Redirect, 400 Bad Request, 401 Unauthorized, 
     # 403 Forbidden, 404 Not Found, 405 Method Not Allowed, 500 Internal Server Error
+    
+    databases = '__all__'
 
     def setUp(self):
         Singleton()
         self.log = get_logger_dashboard('DashBoardViews')      
 
         # Formularios básicos de prueba
-        self.form_1_ok = {'num_acciones': 100, 'precio_compra': 10.5,}      
-        self.form_2_ok = {'precio_entrada_deseado': 15.5,}
+        self.form_1_ok = {'ticker': 'AAPL','fecha_compra': '09/01/2024', 'num_acciones': 50, 'precio_compra': float(148)}
+        self.form_2_ok = {'ticker': 'AAPL', 'precio_entrada_deseado': 130.72}
         self.form_vacío = {}       
         # Formulario en entorno real (no habrá registros en la BD)
         self.form_3_no_ok = {'ticker': 'AAPL','fecha_compra': '02/02/1930', 'num_acciones': 50, 'precio_compra': float(148)}
 
         self.usuarioTest = User.objects.create_user(username='usuario', password='p@ssword')
 
-        self.fecha = tz.now() - timedelta(days=5)
-        self.stockComprado_1 = StockComprado.objects.create(
-            usuario=self.usuarioTest,
-            ticker_bd='ACS_MC',
-            bd='ibex35',
-            ticker='ACS.MC',
-            nombre_stock='ACS, Actividades de Construcción y Servicios, S.A.',
-            fecha_compra=self.fecha,
-            num_acciones=10,
-            precio_compra=100.5,
-            moneda='EUR',
-            sector='Industrials'
-        )
-
-        self.stockSeguimiento_1 = StockSeguimiento.objects.create(
-            usuario=self.usuarioTest,
-            ticker_bd='ACS_MC',
-            bd='ibex35',
-            ticker='ACS.MC',
-            nombre_stock='ACS, Actividades de Construcción y Servicios, S.A.',
-            fecha_inicio_seguimiento=tz.now(),
-            precio_entrada_deseado=15.5,
-            moneda='EUR',
-            sector='Industrials'
-        )
+        
     
+    def test_views_dashboard_status_code_sin_login(self):
+        response = self.client.get('/dashboard/')
+        self.assertEqual(response.status_code, 302, " - [OK] Que página de 'DashBoard' retorne '302...' sin login'")
+        self.log.info(" - [OK] Que página de 'DashBoard' retorne '302...' sin login'")
 
-    def test_views_nueva_compra_sin_login(self):
+
+    # ------------------
+    # TESTS NUEVA COMPRA
+    # ------------------
+    def test_views_nueva_compra_status_code_sin_login(self):
         response = self.client.get('/dashboard/nueva_compra/')
         self.assertEqual(response.status_code, 302, " - [NO OK] Que página de 'nueva_compra' retorne '302...' sin login")
         self.log.info(" - [OK] Que página de 'nueva_compra' retorne '302...' sin login'")
 
 
-    def test_views_nueva_compra_con_login(self):
+    def test_views_nueva_compra_status_code_con_login(self):
         # Fuerzo el login de un usuario
         self.client.force_login(self.usuarioTest)
         response = self.client.get('/dashboard/nueva_compra/')
@@ -94,10 +70,103 @@ class TestDashBoardViews(TestCase):
         self.log.info(" - [OK] Que página de 'nueva_compra' retorne '200 ok' con login")
 
 
-    def test_views_template_válida_nueva_compra(self):
-        response = self.client.get('/dashboard/nueva_compra/')
-        self.assertTemplateUsed(response, 'nueva_compra.html', " - [NO OK] Asignar 'template' de nueva compra")
-        self.log.info(" - [OK] Asignar 'template' de nueva compra")
+    def test_views_nueva_compra_templates_válidas(self):   
+        # Fuerzo el login de un usuario
+        self.client.force_login(self.usuarioTest)
+        response = self.client.get('/dashboard/nueva_compra/')   
+        self.assertTemplateUsed(response, 'nueva_compra.html', " - [NO OK] Asignar 'template' de nueva_compra y cargar base.html")
+        self.assertTemplateUsed(response, 'base.html', " - [NO OK] Asignar 'template' de nueva_compra y cargar base.html")
+        self.log.info(" - [OK] Asignar 'template' de nueva_compra y cargar base.html")
+    
 
+    def test_views_nueva_compra_post_sin_login(self):
+        response = self.client.post('/dashboard/nueva_compra/', data=self.form_1_ok)
+        self.assertEqual(response.status_code, 302, " - [NO OK] Que POST a 'nueva_compra' retorne '302...' sin login")
+        self.log.info(" - [OK] Que POST a 'nueva_compra' retorne '302...' sin login")
+
+
+    def test_views_nueva_compra_post_con_login(self):
+        self.client.force_login(self.usuarioTest)
+        response = self.client.post('/dashboard/nueva_compra/', data=self.form_1_ok)
+        self.assertEqual(response.status_code, 200, " - [NO OK] Que POST a 'nueva_compra' retorne '200 ok' con login")
+        self.log.info(" - [OK] Que POST a 'nueva_compra' retorne '200 ok' con login")
+
+
+    def test_views_nueva_compra_post_con_datos_no_validos(self):
+        self.client.force_login(self.usuarioTest)
+        response = self.client.post('/dashboard/nueva_compra/', data=self.form_3_no_ok)
+        self.assertEqual(response.status_code, 200, " - [NO OK] Que post a 'nueva_compra' retorne '200 ok' con datos no válidos")
+        self.assertContains(response, 'no existen registros')
+        self.log.info(" - [OK] Que post a 'nueva_compra' retorne '200 ok' con datos no válidos")
+
+
+    # ----------------------
+    # TESTS ELIMINAR COMPRAS
+    # ----------------------
+    def test_views_eliminar_compras_status_code_sin_login(self):
+        response = self.client.get('/dashboard/eliminar_compras/')
+        self.assertEqual(response.status_code, 302, " - [NO OK] Que página de 'eliminar_compras' retorne '302...' sin login")
+        self.log.info(" - [OK] Que página de 'eliminar_compras' retorne '302...' sin login'")
+
+
+    def test_views_eliminar_compras_status_code_con_login(self):
+        # Fuerzo el login de un usuario
+        self.client.force_login(self.usuarioTest)
+        response = self.client.get('/dashboard/eliminar_compras/')
+        self.assertEqual(response.status_code, 200, " - [NO OK] Que página de 'eliminar_compras' retorne '200 ok' con login")
+        self.log.info(" - [OK] Que página de 'eliminar_compras' retorne '200 ok' con login")
 
     
+    def test_views_eliminar_compras_templates_válidas(self):   
+        # Fuerzo el login de un usuario
+        self.client.force_login(self.usuarioTest)
+        response = self.client.get('/dashboard/eliminar_compras/')   
+        self.assertTemplateUsed(response, 'eliminar_compras.html', " - [NO OK] Asignar 'template' de eliminar_compras y cargar base.html")
+        self.assertTemplateUsed(response, 'base.html', " - [NO OK] Asignar 'template' de eliminar_compras y cargar base.html")
+        self.log.info(" - [OK] Asignar 'template' de eliminar_compras y cargar base.html")
+    
+
+    def test_views_eliminar_compras_post_sin_login(self):
+        response = self.client.post('/dashboard/eliminar_compras/', data=self.form_1_ok)
+        self.assertEqual(response.status_code, 302, " - [NO OK] Que POST a 'eliminar_compras' retorne '302...' sin login")
+        self.log.info(" - [OK] Que POST a 'eliminar_compras' retorne '302...' sin login")
+    
+
+    # -----------------------
+    # TESTS NUEVO SEGUIMIENTO
+    # -----------------------
+    def test_views_nuevo_seguimiento_status_code_sin_login(self):
+        response = self.client.get('/dashboard/nuevo_seguimiento/')
+        self.assertEqual(response.status_code, 302, " - [NO OK] Que página de 'nuevo_seguimiento' retorne '302...' sin login")
+        self.log.info(" - [OK] Que página de 'nuevo_seguimiento' retorne '302...' sin login'")
+
+
+    def test_views_nuevo_seguimiento_status_code_con_login(self):
+        # Fuerzo el login de un usuario
+        self.client.force_login(self.usuarioTest)
+        response = self.client.get('/dashboard/nuevo_seguimiento/')
+        self.assertEqual(response.status_code, 200, " - [NO OK] Que página de 'nuevo_seguimiento' retorne '200 ok' con login")
+        self.log.info(" - [OK] Que página de 'nuevo_seguimiento' retorne '200 ok' con login")
+
+
+    def test_views_nuevo_seguimiento_templates_válidas(self):   
+        # Fuerzo el login de un usuario
+        self.client.force_login(self.usuarioTest)
+        response = self.client.get('/dashboard/nuevo_seguimiento/')   
+        self.assertTemplateUsed(response, 'nuevo_seguimiento.html', " - [NO OK] Asignar 'template' de nuevo_seguimiento y cargar base.html")
+        self.assertTemplateUsed(response, 'base.html', " - [NO OK] Asignar 'template' de nuevo_seguimiento y cargar base.html")
+        self.log.info(" - [OK] Asignar 'template' de nuevo_seguimiento y cargar base.html")
+    
+
+    def test_views_nuevo_seguimiento_post_sin_login(self):
+        response = self.client.post('/dashboard/nuevo_seguimiento/', data=self.form_1_ok)
+        self.assertEqual(response.status_code, 302, " - [NO OK] Que POST a 'nuevo_seguimiento' retorne '302...' sin login")
+        self.log.info(" - [OK] Que POST a 'nuevo_seguimiento' retorne '302...' sin login")
+
+
+    def test_views_nuevo_seguimiento_post_con_datos_no_validos(self):
+        self.client.force_login(self.usuarioTest)
+        response = self.client.post('/dashboard/nuevo_seguimiento/', data=self.form_3_no_ok)
+        self.assertEqual(response.status_code, 200, " - [NO OK] Que post a 'nuevo_seguimiento' retorne '200 ok' con datos no válidos y haya 'Error'")
+        self.assertContains(response, 'Error')
+        self.log.info(" - [OK] Que post a 'nuevo_seguimiento' retorne '200 ok' con datos no válidos y haya 'Error'")
