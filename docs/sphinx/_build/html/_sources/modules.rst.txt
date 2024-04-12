af82186d170788a0b7534fefe3a6424748ff606d
rmt0009alu <rodmertov@gmail.com>
2024-04-12 12:26:32 +0200

    Mejoras en conf.py y modules.rst para documentar métodos privados

--- docs/sphinx/_build/html/_sources/modules.rst.txt
+++ docs/sphinx/_build/html/_sources/modules.rst.txt
@@ -3,21 +3,25 @@ Analysis
 
 .. automodule:: Analysis.apps
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
 
 .. automodule:: Analysis.models
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
 
 .. automodule:: Analysis.urls
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
 
 .. automodule:: Analysis.views
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
 
@@ -27,26 +31,31 @@ DashBoard
 
 .. automodule:: DashBoard.apps
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
 
 .. automodule:: DashBoard.forms
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
 
 .. automodule:: DashBoard.models
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
 
 .. automodule:: DashBoard.urls
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
 
 .. automodule:: DashBoard.views
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
 
@@ -56,21 +65,25 @@ FAT
 
 .. automodule:: FAT.asgi
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
 
 .. automodule:: FAT.settings
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
 
 .. automodule:: FAT.urls
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
 
 .. automodule:: FAT.wsgi
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
 
@@ -80,6 +93,7 @@ FAT.routers
 
 .. automodule:: FAT.routers.router_bases_datos
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
 
@@ -89,21 +103,25 @@ Lab
 
 .. automodule:: Lab.apps
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
 
 .. automodule:: Lab.forms
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
 
 .. automodule:: Lab.urls
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
 
 .. automodule:: Lab.views
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
 
@@ -113,15 +131,18 @@ News
 
 .. automodule:: News.apps
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
 
 .. automodule:: News.urls
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
 
 .. automodule:: News.views
    :members:
+   :private-members:
    :undoc-members:
    :show-inheritance:
--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -1,19 +1,62 @@
+"""
+Archivo de configuración de Sphinx.
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
 import os
 import sys
 import django
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('../..'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'FAT.settings'
 django.setup()
 
-import sphinxcontrib_django
-import sphinx_rtd_theme
 
-# -- Configurar el tema de Read The Docs ----------------------------------
-# -------------------------------------------------------------------------
+# -- Información del proyecto -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+project = 'FAT: Financial Analysis Tool'
+copyright = '2024, Rodrigo Merino Tovar'
+author = 'Rodrigo Merino Tovar'
+release = 'v3.0.0.'
+
+# -- Configuración general --------------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'es'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    # Para métodos privados typehints
+    'sphinx.ext.autodoc.typehints',
+    'sphinx.ext.doctest',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.imgmath',
+    'sphinxcontrib_django',
+]
+
+# Para que los métodos aparezcan en el orden del source
+autodoc_member_order = 'bysource'
+
+# Extensión de autodoc de Sphinx
+autodoc_mock_imports = ['django']
+
+# Para métodos privados
+autodoc_typehints = 'description'
+autodoc_class_signature = 'separated'
+
+# Extensión de coverage de Sphinx
+coverage_ignore_functions = ['runserver', 'runtest', 'test']
+
+
+# -- Configurar el tema de Read The Docs ------------------------------------------
+# ---------------------------------------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_static_path = ['_static']
 
-# Configure the Sphinx RTD theme
+# Configurar opciones del tema
 html_theme_options = {
     'canonical_url': '',
     'analytics_id': '',
@@ -23,55 +66,6 @@ html_theme_options = {
     'style_external_links': True,
     'vcs_pageview_mode': '',
     'style_nav_header_background': '#262b2e',
-    'style_nav_header_bottom_border': '1px solid #262b2e',
-    'style_highlight_palette': [
-        '#00495e', '#0086b3', '#c0504d', '#f79646', '#e0ab18', '#6d9e41', 
-        '#d0b48e', '#fdfdc5'
-    ],
-    'style_copyright_background': '#2d2f33'
+    # 'style_nav_header_bottom_border': '1px solid #262b2e',
 }
 
-
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'FAT: Financial Analysis Tool'
-copyright = '2024, Rodrigo Merino Tovar'
-author = 'Rodrigo Merino Tovar'
-release = 'v3.0.0.'
-
-
-# Configure the Sphinxcontrib Django extension
-# sphinxcontrib_django.extension(
-#     settings_module='FAT.settings',
-#     apps=['FAT'],
-#     ignore_patterns=['migrations', 'templates']
-# )
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.imgmath',
-    'sphinxcontrib_django',
-]
-
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-language = 'es'
-
-# Configure the Sphinx autodoc extension
-autodoc_mock_imports = ['django']
-
-# Configure the Sphinx coverage extension
-coverage_ignore_functions = ['runserver', 'runtest', 'test']
