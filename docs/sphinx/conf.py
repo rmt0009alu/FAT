a2c7186202c14bc49ea31d68ee16bcc85b974d55
rmt0009alu <rodmertov@gmail.com>
2024-04-12 11:02:11 +0200

    Actualizar conf.py

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import django
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('..\..'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'FAT.settings'
 django.setup()
 
